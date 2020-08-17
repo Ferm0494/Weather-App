6534f8f8537ce9014c8fd8b609250127816b363a
Ferm0494 <ferm0494@gmail.com>
2020-08-17 12:01:00 -0600

    Add toggle C/F

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "bootstrap";
+import "./assets/stylesheets/toggle.scss"
 import "./assets/stylesheets/index.scss";
 import './assets/stylesheets/weatherBackgrounds.scss'
-import {
-    sun
-} from "./assets/js/gifs";
+
 import {
     init
 } from './assets/js/location'
